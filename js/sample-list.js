9f27dc4d224b06b7eb05a534a7fac1321adffbaa
Palti Gorat <palti.gorat@doku.com>
2021-03-13 13:35:12 +0700

    new page UDM-sample Riwayat jabatan

--- js/sample-list.js
+++ js/sample-list.js
@@ -1,5 +1,5 @@
 const listUDM = [
-  {id: '1', name: 'Riwayat Jabatan'},
+  {id: '1', name: 'Riwayat Jabatan', url:'riwayat-jabatan/list-riwayat-jabatan.html'},
   {id: '2', name: 'Riwayat Pendidikan'},
   {id: '3', name: 'Riwayat SKP'},
   {id: '4', name: 'Riwayat Diklat'},
@@ -19,8 +19,38 @@ const listUDM = [
   {id: '4', name: 'Riwayat Kepanitiaan'}
 ]
 
-for(i=0; i<listUDM.length; i++){
-  var title = listUDM[i].name;
-  document.getElementById("loopHere").innerHTML += ' <div class="list-item"> <div class="me-3">  <i class="bi bi-grid-3x3-gap-fill"></i> </div><div class="">'+title+'</div><div class="ms-auto"><i class="bi bi-chevron-right"></i></div></div>'
+function navigateTo(here){
+  console.log(here);
+  window.location.href = here;
+};
+
+// goo = (e)=>{
+//   e.preventDefault();
+//   alert('hehe');
+// }
+
+const loopList = document.getElementById("loopHere");
+const loopGrid = document.getElementById("loopGridHere");
+if(document.body.contains(loopList)){
+  for(i=0; i<listUDM.length; i++){
+    var title = listUDM[i].name;
+    var link = listUDM[i].url;
+    loopList.innerHTML += ' <a class="list-item" href="'+link+'"> <div class="me-3">  <i class="bi bi-grid-3x3-gap-fill"></i> </div><div class="">'+title+'</div><div class="ms-auto"><i class="bi bi-chevron-right"></i></div></a>'
+  }
 }
 
+
+if(document.body.contains(loopGrid)){
+  for(i=0 ; i<listUDM.length; i++){
+    var title = listUDM[i].name;
+    var link = listUDM[i].url;
+    loopGrid.innerHTML += '<div class="widget widget-light" ><div class="widget-body"><div class="widget-icon"><i class="bi bi-archive"></i></div><div class="pb-3">'+title+'</div></div></div>'
+  }
+}
+
+function navigateTo(here){
+  window.location.href = here
+};
+goo = ()=>{
+  alert('hehe')
+}
