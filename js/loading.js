fb8a867c1b9e6aeae2dae5bb7abd5eed1957ab0d
Palti Gorat <palti.gorat@doku.com>
2021-04-10 22:32:52 +0700

    fix animation rules for dropdown color, add loading screen (sample on Dashboard)

---
+++ js/loading.js
@@ -0,0 +1,21 @@
+// const loadingComponent = document.getElementById("loading");
+loadingComp = `<div id="loading-content"> <img class="loading-img" src="../../assets/images/logogram_login.svg" alt=""> Mohon tunggu  </div>`;
+
+if (document.body.classList.contains("loading")) {
+  console.log('ada loading');
+  // setTimeout(function () {
+  // }, 3000);
+  var loadingComponent = document.createElement("div");
+  loadingComponent.id = 'loading';
+  loadingComponent.innerHTML = loadingComp
+  document.body.appendChild(loadingComponent)
+  loadingComponent.style.overflow="hidden";
+  document.body.style.overflow="hidden";
+
+} else {
+  loadingComponent.remove();
+  document.body.style.overflow="auto";
+
+
+
+}
